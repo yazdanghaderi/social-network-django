914d378a1964f262b39661d45c9edec912c79d57
yazdanghaderi <yazdanghaderi70@gmail.com>
2022-05-09 15:55:20 +0430

    Posts

---
+++ A/account/authenticate.py
@@ -0,0 +1,29 @@
+
+from django.contrib.auth.models import User
+
+class EmailBackend:
+    """
+    Authenticate against the settings ADMIN_LOGIN and ADMIN_PASSWORD.
+
+    Use the login name and a hash of the password. For example:
+from django.contrib.auth.hashers import check_password
+
+    ADMIN_LOGIN = 'admin'
+    ADMIN_PASSWORD = 'pbkdf2_sha256$30000$Vo0VlMnkR4Bk$qEvtdyZRWTcOsCnI/oQ7fVOu1XAURIZYoOZ3iq8Dr4M='
+    """
+
+    def authenticate(self, request, username=None, password=None):
+
+        try:
+            user = User.objects.get(email=username)
+            if user.check_password(password):
+                return user
+            return None
+        except User.DoesNotExist:
+            return None
+
+    def get_user(self, user_id):
+        try:
+            return User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            return None
