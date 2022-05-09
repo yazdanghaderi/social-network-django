df101c30b84de378f84f10b57e6864ca1c75ed70
yazdanghaderi <yazdanghaderi70@gmail.com>
2022-05-09 16:13:50 +0430

    customizing admin

--- A/home/admin.py
+++ A/home/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 from .models import Post
 # Register your models here.
 
-admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("user", "slug", "updated")
+    search_fields = ("slug", "body")
+    list_filter = ("updated",)
+    prepopulated_fields = {"slug":("body",)}
+    raw_id_fields = ("user",)
+
+admin.site.register(Post, PostAdmin)
